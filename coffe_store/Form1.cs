1e8010195481a82db17db6b99069b8280134379b
hoanganh <hoang09012004xn@gmail.com>
2024-10-30 22:40:43 +0700

    firstpush

---
+++ coffe_store/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coffe_store
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+                int borderWidth = 2; // Độ dày đường viền
+                Color borderColor = Color.Red; // Màu đường viền
+                ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+                                        borderColor, borderWidth, ButtonBorderStyle.Solid,
+                                        borderColor, borderWidth, ButtonBorderStyle.Solid,
+                                        borderColor, borderWidth, ButtonBorderStyle.Solid,
+                                        borderColor, borderWidth, ButtonBorderStyle.Solid);
+        }
+    }
+}
